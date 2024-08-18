a542c5c0a95c8a9281929086a2e22cb1afaf6840
iatrii <ishanatrii@protonmail.com>
2024-08-18 18:17:33 -0400

    editing for lint again last try

--- service/routes.py
+++ service/routes.py
@@ -58,12 +58,11 @@ def create_accounts():
     )
 
 
-
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
 
-# ... place you code here to LIST accounts ...
+
 
 @app.route("/accounts", methods=["GET"])
 def list_all_accounts():
@@ -78,7 +77,6 @@ def list_all_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-# ... place you code here to READ an account ...
 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
@@ -95,7 +93,6 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -114,7 +111,6 @@ def update_accounts(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
-# ... place you code here to DELETE an account ...
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
